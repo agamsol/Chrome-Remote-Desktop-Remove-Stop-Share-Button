375695647b1d90d6a1882c9de8734b11ee3252b3
agamsol <43265196+agamsol@users.noreply.github.com>
2022-09-13 13:45:24 +0300

    Official Release | Remove-StopButton Version 1.0 | Fixed install path detector and set curl output to silent

--- Remove-StopShare-Button.bat
+++ Remove-StopShare-Button.bat
@@ -14,9 +14,16 @@ set "REMOTE_CORE=https://raw.githubusercontent.com/agamsol/Chrome-Remote-Desktop
 
 
 for %%a in ("!ProgramFiles(x86)!" "!ProgramFiles!") do (
-    if exist "%%~a\Google\Chrome Remote Desktop\CurrentVersion" (
-        call :CHROME_REMOTE_DESKTOP_INSTALLED "%%~a\Google\Chrome Remote Desktop\CurrentVersion"
+    if exist "%%~a\Google\Chrome Remote Desktop" (
+        if exist "%%~a\Google\Chrome Remote Desktop\CurrentVersion" (
+            call :CHROME_REMOTE_DESKTOP_INSTALLED "%%~a\Google\Chrome Remote Desktop\CurrentVersion"
+        ) else (
+            for /f "delims=" %%b in ('dir /b "%%~a\Google\Chrome Remote Desktop\*"') do (
+                call :CHROME_REMOTE_DESKTOP_INSTALLED "%%~a\Google\Chrome Remote Desktop\%%~b"
+            )
+        )
         echo Process has been finished
+        pause
         exit /b 0
     )
 )
@@ -38,16 +45,15 @@ del /q "!CRD_PATH!\remoting_core.dll" && (
     echo     [+] INFO: Successfully deleted remote_core.dll
 )
 
-curl -Lko "!CRD_PATH!\remoting_core.dll" "!REMOTE_CORE!" && (
+curl -sLko "!CRD_PATH!\remoting_core.dll" "!REMOTE_CORE!" && (
     echo:
     echo     [+] INFO: Successfully downloaded the new remoting-core
 )
 
 if exist "!CRD_PATH!\remoting_host.exe" (
-    start "" "!CRD_PATH!\remoting_host.exe" && (
-        echo:
-        echo     [+] INFO: Successfully launched chrome-remote-desktop.
-    )
+    "!CRD_PATH!\remoting_host.exe"
+    echo:
+    echo     [+] INFO: Successfully launched chrome-remote-desktop.
 )
 
 echo:
