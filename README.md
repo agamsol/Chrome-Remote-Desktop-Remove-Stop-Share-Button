29f3da829d8682124438e246c498fdf71dd2bebd
agamsol <43265196+agamsol@users.noreply.github.com>
2022-09-13 13:24:41 +0300

    Official Release | Remove-StopButton Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Chrome-Remote-Desktop-Remove-Stop-Share-Button
-Remove the stop share button from chrome remote desktop by launching a small script which is going to perform a few functions to remove the button
+# Remove-StopShare-Button Version 1.0
+> Remove the stop share button from chrome remote desktop by launching a small script which is going to perform a few functions to remove the button
+
+![IMAGE](https://cdn.agamsol.xyz:90/media/Code_7u1y8nNaRL.png)
+
+### ⚠️ NOTES:
+- This script should be working on windows 10 and above
+- **FOR LOWER END OPERATING SYSTEMS:** curl.exe is included in this repository for you.
+
+### ⏬ Download
+- Download the file [`Remove-StopShare-Button.bat`](https://github.com/agamsol/Chrome-Remote-Desktop-Remove-Stop-Share-Button/blob/1.0/Remove-StopShare-Button.bat)
+- Run the file __as administrator__
+- Connect without stopshare button :smile:
---
+++ Remove-StopShare-Button.bat
@@ -0,0 +1,58 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set VERSION=1.0
+set "REMOTE_CORE=https://raw.githubusercontent.com/agamsol/Chrome-Remote-Desktop-Remove-Stop-Share-Button/!VERSION!/remoting_core.dll"
+
+:: CHECK EVALATIONS
+>nul 2>&1 net session || (
+    echo:
+    echo ERROR: Please run this script as administrator
+    >nul timeout /t 5
+    exit /b 1
+)
+
+
+for %%a in ("!ProgramFiles(x86)!" "!ProgramFiles!") do (
+    if exist "%%~a\Google\Chrome Remote Desktop\CurrentVersion" (
+        call :CHROME_REMOTE_DESKTOP_INSTALLED "%%~a\Google\Chrome Remote Desktop\CurrentVersion"
+        echo Process has been finished
+        exit /b 0
+    )
+)
+echo:
+echo ERROR: Chrome remote desktop is not installed
+>nul timeout /t 5
+exit /b 1
+
+:CHROME_REMOTE_DESKTOP_INSTALLED [PATH]
+set "CRD_PATH=%~1"
+set PROCESSES_ENDED=0
+
+for /f "delims=" %%a in ('dir /b "!CRD_PATH!\*.exe"') do >nul 2>&1 taskkill /f /im "%%a" && set /a PROCESSES_ENDED+=1
+echo:
+echo     [+] INFO: Successfully killed !PROCESSES_ENDED! process(es)
+
+del /q "!CRD_PATH!\remoting_core.dll" && (
+    echo:
+    echo     [+] INFO: Successfully deleted remote_core.dll
+)
+
+curl -Lko "!CRD_PATH!\remoting_core.dll" "!REMOTE_CORE!" && (
+    echo:
+    echo     [+] INFO: Successfully downloaded the new remoting-core
+)
+
+if exist "!CRD_PATH!\remoting_host.exe" (
+    start "" "!CRD_PATH!\remoting_host.exe" && (
+        echo:
+        echo     [+] INFO: Successfully launched chrome-remote-desktop.
+    )
+)
+
+echo:
+echo     [+] INFO: You may now connect and you should'nt notice a Stop-Share button
+echo:
+echo     [*] Press any key to EXIT the script
+pause >nul
+exit /b 0
